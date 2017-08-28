d3babb72289be7fc19cb232ea530cdb7f9c8cbb4
Bhoomika <>
2017-08-28 19:20:08 +0530

    Ignore nbproject netbeans files

--- libs/zipper.php
+++ libs/zipper.php
@@ -142,7 +142,7 @@ class zipper {
     public function make_zip($album_download_directory) {
         $zipfilename = "";
         if (isset($album_download_directory)) {
-            $zipfilename = 'albums/fb-album_' . date("Y-m-d") . '_' . date("H-i-s");
+            $zipfilename = 'albums/fb-album_' . date("Y-M-d") . '_' . date("H-i-s");
 
             // name of folder starting from the root of the webserver
            
